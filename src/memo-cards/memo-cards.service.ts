155eba91173644ee9a7249302bcf9014861daf41
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-12-01 20:57:41 +0200

    Leave todo comment for more detailed error handling in the future

--- src/memo-cards/memo-cards.service.ts
+++ src/memo-cards/memo-cards.service.ts
@@ -58,6 +58,7 @@ export class MemoCardsService {
         include: { category: true },
       });
     } catch {
+      // TODO: distinguish between not found and other errors based on maping Prisma error code to HTTP status codes
       throw new NotFoundException(`Memo card with ID ${id} not found`);
     }
   }
@@ -68,6 +69,7 @@ export class MemoCardsService {
         where: { id },
       });
     } catch {
+      // TODO: distinguish between not found and other errors based on maping Prisma error code to HTTP status codes
       throw new NotFoundException(`Memo card with ID ${id} not found`);
     }
   }
