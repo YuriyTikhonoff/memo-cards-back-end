53555b49b44aa2355b5a2cbd61495ff50f9353a4
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-12-04 00:25:23 +0200

    Remove mock data

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,18 +1,11 @@
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CategoryDto } from './dto/category.dto';
 import { Injectable } from '@nestjs/common';
-// import { generatedMockAsyncRequest } from 'src/utils/mock';
 
 @Injectable()
 export class CategoriesService {
   constructor(private readonly prismaService: PrismaService) {}
   async getCategories(): Promise<CategoryDto[]> {
-    // return await generatedMockAsyncRequest([
-    //   { id: '550e8400-e29b-41d4-a716-446655440000', name: 'Math' },
-    //   { id: '550e8400-e29b-41d4-a716-446655440001', name: 'Science' },
-    //   { id: '550e8400-e29b-41d4-a716-446655440002', name: 'History' },
-    //   { id: '550e8400-e29b-41d4-a716-446655440003', name: 'Language' },
-    // ] as CategoryDto[]);
     return await this.prismaService.category.findMany();
   }
 
--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,6 +1,6 @@
 const DEFAULT_DELAY_MS = 1000;
 
-export const generatedMockAsyncRequest = async <T>(
+export const generateMockAsyncRequest = async <T>(
   payload: T,
   delayInMilliseconds = DEFAULT_DELAY_MS,
 ): Promise<T> => {
