d4cc96dd0ebcff9e17666eb8ebb0ee93a5df97c6
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-12-26 00:25:55 +0200

    Update info

--- src/memo-cards/memo-cards.controller.ts
+++ src/memo-cards/memo-cards.controller.ts
@@ -20,7 +20,7 @@ import { ROUTES } from 'src/constants';
 // GET /memo-cards - Get all memo cards (optionally filter by ?categoryId=xxx)
 // GET /memo-cards/practice?limit=10 - Get cards due for practice
 // GET /memo-cards/:id - Get one memo card
-// PATCH /memo-cards/:id - Update a memo card
+// PUT /memo-cards/:id - Update a memo card
 // PATCH /memo-cards/:id/practice - Update practice timestamp and level
 // DELETE /memo-cards/:id - Delete a memo card
 
